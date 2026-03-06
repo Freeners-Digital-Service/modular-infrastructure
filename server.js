5045b20c382f1e4f1d87fb723bb7c84ebae04400
Freeners Digital Services <freenersdigitals@gmail.com>
2026-03-06 22:10:49 +0100

    enable cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 /* Root route */
