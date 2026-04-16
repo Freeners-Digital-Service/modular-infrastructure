eeefbb7ab0ad6e8d78ca7052356b98b273c0d531
Freeners Digital Services <freenersdigitals@gmail.com>
2026-04-16 17:18:34 +0100

    test: multer + debug file upload

--- server.js
+++ server.js
@@ -1062,8 +1062,9 @@ app.get("/api/setup/load", async (req, res) => {
    SUBMIT SETUP
 ========================= */
 
-app.post("/api/setup/submit", async (req, res) => {
+app.post("/api/setup/submit", upload.single("logo_file"), async (req, res) => {
   try {
+    console.log("FILE:", req.file);
 
     const logoPath = req.file ? `/uploads/${req.file.filename}` : null;
     
