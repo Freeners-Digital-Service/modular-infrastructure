8c3a6d0a5b7f079746f16309c0c5a8cf46e3e99d
Freeners Digital Services <freenersdigitals@gmail.com>
2026-05-31 23:18:08 +0100

    Update client agents target structure

--- routes/adminClientAgents.js
+++ routes/adminClientAgents.js
@@ -14,21 +14,20 @@ function adminClientAgents(pool, renderPage) {
           ca.id,
           ca.client_id,
           ca.system_id,
-          ca.module_id,
+          ca.target_type,
           ca.agent_id,
           ca.status,
           ca.activated_at,
 
           c.name AS client,
           s.name AS system,
-          m.name AS module,
+          ca.target_type AS target,
           a.name AS agent
 
         FROM client_agents ca
 
         LEFT JOIN clients c ON ca.client_id = c.id
         LEFT JOIN systems s ON ca.system_id = s.id
-        LEFT JOIN modules m ON ca.module_id = m.id
         LEFT JOIN agents a ON ca.agent_id = a.agent_id
    
 
@@ -40,7 +39,7 @@ function adminClientAgents(pool, renderPage) {
           <td>${r.id}</td>
           <td>${r.client || r.client_id}</td>
           <td>${r.system || r.system_id}</td>
-          <td>${r.module || r.module_id}</td>
+          <td>${r.target_type || "-"}</td>
           <td>${r.agent || r.agent_id}</td>
           <td>${r.status}</td>
           <td>${r.activated_at}</td>
@@ -53,7 +52,7 @@ function adminClientAgents(pool, renderPage) {
             <th>ID</th>
             <th>Client</th>
             <th>System</th>
-            <th>Module</th>
+            <th>Target</th>
             <th>Agent</th>
             <th>Status</th>
             <th>Activated At</th>
